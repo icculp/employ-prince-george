e4f1b19dc152541ec58dac58cc6c9b12257c00c4
Ian Culp <icculp@gmail.com>
2022-02-13 23:05:26 -0600

    suite to text

--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -45,6 +45,7 @@ function Form() {
       'Retail',
       'Restaurant',
       'IT',
+      'Logistics',
       'Transportation',
       'Manufacturing',
       'Professional',
@@ -104,7 +105,7 @@ function Form() {
       <input type="text" placeholder="Street" {...register("street", {required: true,  pattern: /^[a-zA-Z1-9 ]+$/, maxLength: 20})} />
       {errors.street && <p class="error">Please check the field above</p>}
       <label>Suite</label>
-      <input type="number" placeholder="Suite" {...register("suite", {required: true, maxLength: 10})} />
+      <input type="text" placeholder="Suite" {...register("suite", {required: true, maxLength: 10})} />
       {errors.suite && <p class="error">Please check the field above</p>}
       <label>City</label>
       <input type="text" placeholder="City" {...register("city", {required: true, pattern: /^[a-zA-Z ]+$/, minLength: 2, maxLength: 20})} />
